22234ede67d5dbc8ac96771d26939c901ce2f605
felixghosh <zanzaniman@gmail.com>
2021-06-11 11:05:04 +0200

    set up glfw and glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-	std::cout << "Hello world! FECK FECK FECK" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
