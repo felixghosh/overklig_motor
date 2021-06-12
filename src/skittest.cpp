07175903dbed0d9f5c90c5a830601f1af8cb6182
AbbeGreen <greenalbin99@gmail.com>
2021-06-12 16:10:28 +0200

    nu kan man bygga kuber!

--- src/skittest.cpp
+++ src/skittest.cpp
@@ -1,7 +1,12 @@
 #include <GL/freeglut.h>
+#include <ctime>
+#include <cstdlib>
+
+GLfloat angle = 0;
+GLfloat col[12];
 
 void renderString(float x, float y, float z, void *font, char *string)
-{
+{ //Doesn't fucking work
     char *c;
     glRasterPos3f(x, y, z);
     for(c = string; *c != '\0'; c++) {
@@ -9,38 +14,97 @@ void renderString(float x, float y, float z, void *font, char *string)
     }
 }
 
-static void renderSceneCB()
+void spin()
 {
-    glClearColor(0.0f, 0.0f, 0.25f, 0.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    angle += 1;
+    if(angle > 360) {
+        angle = 0;
+    }
+    glutPostRedisplay();
+}
 
-    glBegin(GL_TRIANGLES);
-    glColor3f(1, 0, 0);
-    glVertex2f(-0.8, -0.8);
-    glColor3f(0, 1, 0);
-    glVertex2f(0.8, -0.8);
-    glColor3f(0, 0, 1);
-    glVertex2f(0, 0.9);
+void init()
+{
+    glClearColor(0, 0, 0, 1);
+    glColor3f(0.5, 0.2, 0.7);
+    glEnable(GL_DEPTH_TEST);
+
+    for (int i = 0; i < 12; i++) {
+        col[i] = (double)rand()/RAND_MAX;
+    }
+}
+
+void face(GLfloat A[], GLfloat B[], GLfloat C[], GLfloat D[])
+{
+    glBegin(GL_POLYGON);
+        glColor3f(col[0], col[1], col[2]);
+        glVertex3fv(A);
+        glColor3f(col[3], col[4], col[5]);
+        glVertex3fv(B);
+        glColor3f(col[6], col[7], col[8]);
+        glVertex3fv(C);
+        glColor3f(col[9], col[10], col[11]);
+        glVertex3fv(D);
     glEnd();
-    glutSwapBuffers();
-    renderString(10.0f, 10.0f, 0.0f, GLUT_BITMAP_TIMES_ROMAN_24, "HAR NI FRÅGOR? HAR NI PROBLEM?");
 }
 
-static void initializeGlutCallbacks()
+void cube(GLfloat V0[], GLfloat V1[], GLfloat V2[], GLfloat V3[], 
+          GLfloat V4[], GLfloat V5[], GLfloat V6[], GLfloat V7[])
 {
-    glutDisplayFunc(renderSceneCB);
+    //glColor3f(1, 0, 0);
+    face(V0, V1, V2, V3);
+
+    //glColor3f(0, 1, 0);
+    face(V4, V5, V6, V7);
+
+    //glColor3f(0, 0, 1);
+    face(V0, V3, V7, V4);
+
+    //glColor3f(0, 1, 1);
+    face(V1, V2, V6, V5);
+
+    //glColor3f(1, 0, 1);
+    face(V0, V1, V5, V4);
+
+    //glColor3f(1, 1, 0);
+    face(V3, V2, V6, V7);
 }
 
+void draw()
+{
+    GLfloat V[8][3] = {
+        {-0.5, 0.5, 0.5},
+        {0.5, 0.5, 0.5},
+        {0.5, -0.5, 0.5},
+        {-0.5, -0.5, 0.5},
 
+        {-0.5, 0.5, -0.5},
+        {0.5, 0.5, -0.5},
+        {0.5, -0.5, -0.5},
+        {-0.5, -0.5, -0.5},
+    };
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLoadIdentity();
+    glRotatef(angle, 1, 1, 1);
+
+    cube(V[0], V[1], V[2], V[3], V[4], V[5], V[6], V[7]);
+
+    glutSwapBuffers();
+}
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(1024, 768);
-    glutInitWindowPosition(400, 600);
+    
+    glutInitWindowSize(1000, 800);
+    glutInitWindowPosition(600, 400);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutCreateWindow("Overklig Motor 1.0");
-    initializeGlutCallbacks();
+    init();
+    glutDisplayFunc(draw);
+    glutIdleFunc(spin);
     glutMainLoop();
     return 0;
 }
